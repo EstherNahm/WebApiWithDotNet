56f70377aefa1fa468b9229eae4924f418258379
esther nahm <efnahm@gmail.com>
2024-11-11 14:29:04 -0500

    initial

---
+++ src/DemoSolution/DemoApi/Status/StatusController.cs
@@ -0,0 +1,24 @@
+﻿namespace HtTemplate.Status;
+
+public class StatusController : ControllerBase
+{
+    [HttpGet("/status")]
+    public async Task<ActionResult> GetTheStatus()
+    {
+        return Ok("This is the status for right now");
+    }
+
+    [HttpGet("/status/{year:int}/{month:int:max(12):min(1)}/{day:int}")]
+    public async Task<ActionResult> GetTheStatus(int year, int month, int day)
+    {
+        return Ok();
+    }
+
+    [HttpGet("/employees")]
+    public async Task<ActionResult> GetEmployees(
+        [FromQuery]
+        string department = "All")
+    {
+        return Ok($"here are {department} the the employees...");
+    }
+}
